1e711801c6a52811a169d2bffc77b260204edf41
gh0st-8221 <darkr.root.system@proton.me>
2026-09-10 22:51:41 +0300

    update backup from

--- src/main.rs
+++ src/main.rs
@@ -43,11 +43,11 @@ fn main() -> std::io::Result<()> {
         }
     }
 
-    let grub_src = Path::new("/etc/default/grub");
-    if grub_src.exists() {
-        let etc_dst = tmp_base.join("etc/default");
-        fs::create_dir_all(&etc_dst)?;
-        fs::copy(grub_src, etc_dst.join("grub"))?;
+    let grub_cfg_src = Path::new("/boot/grub/grub.cfg");
+    if grub_cfg_src.exists() {
+        let grub_dst = tmp_base.join("boot/grub");
+        fs::create_dir_all(&grub_dst)?;
+        fs::copy(grub_cfg_src, grub_dst.join("grub.cfg"))?;
     }
 
     let grub_theme_src = Path::new("/usr/share/grub/themes/catppuccin-mocha-grub-theme");
@@ -69,16 +69,16 @@ fn main() -> std::io::Result<()> {
         cd ~/git/driftwm\n\
         make build\n\
         sudo make install\n\n\
-        if [ -d ~/git/driftwm-dotfiles/usr/share/grub/themes/theme ]; then\n\
-            sudo mkdir -p /usr/share/grub/themes/theme\n\
-            sudo cp -r ~/git/driftwm-dotfiles/usr/share/grub/themes/catppuccin-mocha-grub-theme/* /usr/share/grub/themes/catppuccin-mocha-grub-theme/\n\
+        if [ -d ~/git/driftwm-dotfiles/usr/share/grub/themes/catppuccin-mocha-grub-theme ]; then\n\
+            sudo mkdir -p /usr/share/grub/themes/catppuccin-mocha-grub-theme\n\
+            sudo cp -r ~/git/driftwm-dotfiles/usr/share/grub/themes/catppuccin-mocha-grub-theme/. /usr/share/grub/themes/catppuccin-mocha-grub-theme/\n\
         fi\n\
-        if [ -f ~/git/driftwm-dotfiles/etc/default/grub ]; then\n\
-            sudo mkdir -p /etc/default\n\
-            sudo cp ~/git/driftwm-dotfiles/etc/default/grub /etc/default/grub\n\
-        fi\n\
-        sudo grub-mkconfig -o /boot/grub/grub.cfg\n\n\
-        cp -r ~/git/driftwm-dotfiles/.config/* ~/.config/\n\
+        if [ -f ~/git/driftwm-dotfiles/boot/grub/grub.cfg ]; then\n\
+            sudo mkdir -p /boot/grub\n\
+            sudo cp ~/git/driftwm-dotfiles/boot/grub/grub.cfg /boot/grub/grub.cfg\n\
+        fi\n\n\
+        mkdir -p ~/.config\n\
+        cp -r ~/git/driftwm-dotfiles/.config/. ~/.config/\n\
         cp ~/git/driftwm-dotfiles/.zshrc ~/.zshrc\n\
         cp ~/git/driftwm-dotfiles/.zprofile ~/.zprofile\n\n\
         chsh -s $(which zsh) $USER\n\
